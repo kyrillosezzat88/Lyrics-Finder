9d5cbe0832bce166ec34e23e7ef66abb2e84d664
kyrillos <kyrillos417@gmail.com>
2020-01-23 01:50:04 -0800

    Updates

---
+++ precache-manifest.bc38f82fcb5cb68cfeb7f5ed61eb7224.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f04e262abd31dbc551ebef5fed37fadc",
+    "url": "/Lyrics-Finder/index.html"
+  },
+  {
+    "revision": "672479829bae7e5346c1",
+    "url": "/Lyrics-Finder/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "aa204b540af2e3ad8c8d",
+    "url": "/Lyrics-Finder/static/css/main.f347ac6c.chunk.css"
+  },
+  {
+    "revision": "672479829bae7e5346c1",
+    "url": "/Lyrics-Finder/static/js/2.b8a6f1a5.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/Lyrics-Finder/static/js/2.b8a6f1a5.chunk.js.LICENSE"
+  },
+  {
+    "revision": "aa204b540af2e3ad8c8d",
+    "url": "/Lyrics-Finder/static/js/main.5b870ea8.chunk.js"
+  },
+  {
+    "revision": "d3d94a10e17cec218268",
+    "url": "/Lyrics-Finder/static/js/runtime-main.56a3487b.js"
+  }
+]);
